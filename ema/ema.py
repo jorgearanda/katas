2b5d6463d324ae916852fb60ca1bcd55e74b16b1
Jorge Aranda <jorge.aranda@cuevano.ca>
2018-08-08 10:58:53 -0700

    Add Exponential Moving Average Kata, second pass

--- ema/ema.py
+++ ema/ema.py
@@ -1,17 +1,29 @@
 class ExponentialMovingAverage():
     def __init__(self, alpha=0.5):
-        self.ema = None
         self.alpha = alpha
-
-    def put(self, input):
-        if type(input) == int:
-            input = [input]
-
-        for i in input:
-            if self.ema is None:
-                self.ema = i
-
-            self.ema = (i * self.alpha) + (self.ema * (1 - self.alpha))
+        self.ema = None
 
     def get(self):
         return self.ema
+
+    def put(self, input):
+        if type(input) is int:
+            input = [input]
+
+        for value in input:
+            self._calc(value)
+
+    def _calc(self, value):
+        if self.ema is None:
+            self.ema = value
+        else:
+            self.ema = value * self.alpha + self.ema * (1.0 - self.alpha)
+
+
+# Second pass. Separating the input processing and the calculation
+# makes for a cleaner structure.
+#
+# It also allows for an alternative approach: `put` could keep a list of
+# values so far, and _calc could use a subset for them for the calculation.
+#
+# This would allow, for instance, an EMA of only the last ten values.
