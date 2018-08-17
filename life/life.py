dc5513ab837898175127053a081e4b84e3c94be7
Jorge Aranda <jorge.aranda@cuevano.ca>
2018-08-17 11:16:32 -0700

    Add Game of Life Kata, second pass

--- life/life.py
+++ life/life.py
@@ -1,59 +1,71 @@
+LIVE = '*'
+DEAD = '.'
+
+
 def life(input):
-    output = []
-    for row_idx, row in enumerate(input):
-        outline = ''
-        for col_idx in range(len(row)):
-            neighbours = _neighbours(input, row_idx, col_idx)
-            if _is_live(input, row_idx, col_idx):
-                if _too_sparse(neighbours) or _too_crowded(neighbours):
-                    outline += '.'
-                else:
-                    outline += '*'
+    game = GameOfLife(input)
+    return game.next()
+
+
+class GameOfLife():
+    def __init__(self, input):
+        self.grid = input
+        self.min_x = 0
+        self.max_x = 0
+        if len(self.grid) > 0:
+            self.max_x = len(self.grid[0]) - 1
+        self.min_y = 0
+        self.max_y = len(self.grid) - 1
+
+    def next(self):
+        self.grid = [
+            ''.join([self._cell_status(x, y) for x in range(len(row))])
+            for y, row in enumerate(self.grid)
+        ]
+
+        return self.grid
+
+    def _cell_status(self, x, y):
+        if self._is_live(x, y):
+            if self._cell_survives(x, y):
+                return LIVE
             else:
-                if _right_for_life(neighbours):
-                    outline += '*'
-                else:
-                    outline += '.'
+                return DEAD
+        else:
+            if self._is_ripe_for_life(x, y):
+                return LIVE
+            else:
+                return DEAD
 
-        output.append(outline)
+    def _cell_survives(self, x, y):
+        return 2 <= self._neighbours(x, y) <= 3
 
-    return output
+    def _is_ripe_for_life(self, x, y):
+        return self._neighbours(x, y) == 3
 
+    def _neighbours(self, x, y):
+        candidates = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 1),
+            (1, -1), (1, 0), (1, 1),
+        ]
+        count = 0
+        for i, j in candidates:
+            if self._is_valid_cell(x + i, y + j) and \
+                    self._is_live(x + i, y + j):
+                count += 1
 
-def _neighbours(input, row_idx, col_idx):
-    count = 0
-    neighbours = [
-        (-1, -1), (-1, 0), (-1, 1),
-        (0, -1), (0, 1),
-        (1, -1), (1, 0), (1, 1)
-    ]
-    for i, j in neighbours:
-        if _are_valid_coordinates(input, row_idx + i, col_idx + j) and \
-                _is_live(input, row_idx + i, col_idx + j):
-            count += 1
+        return count
 
-    return count
+    def _is_valid_cell(self, x, y):
+        return self.min_x <= x <= self.max_x and \
+            self.min_y <= y <= self.max_y
 
+    def _is_live(self, x, y):
+        return self.grid[y][x] == LIVE
 
-def _are_valid_coordinates(grid, row, col):
-    return (row >= 0 and row < len(grid) and col >= 0 and col < len(grid[row]))
-
-
-def _is_live(grid, row, col):
-    return grid[row][col] == '*'
-
-
-def _too_sparse(count):
-    return count < 2
-
-
-def _too_crowded(count):
-    return count > 3
-
-
-def _right_for_life(count):
-    return count == 3
-
-
-# First pass. I think this will benefit from a class-based approach,
-# which I should implement the next time around.
+# Second pass. I think I went a little crazy with list comprehensions here;
+# the result being maybe less readable than my first pass.
+# I should have probably added another class for cells: so many of the
+# methods above are for a single cell and it may make sense to
+# isolate that next time.
