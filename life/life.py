5c659ebd757a2d58db8a459e5f77c724ff65d8e4
Jorge Aranda <jorge.aranda@cuevano.ca>
2018-08-20 11:30:55 -0700

    Add Game of Life Kata, third pass

--- life/life.py
+++ life/life.py
@@ -1,71 +1,121 @@
-LIVE = '*'
-DEAD = '.'
+from enum import Enum
+
+
+class State(Enum):
+    DEAD = 0
+    LIVE = 1
+
+
+class Outlook(Enum):
+    LONELY = 0
+    STABLE = 1
+    FERTILE = 2
+    CROWDED = 3
 
 
 def life(input):
-    game = GameOfLife(input)
-    return game.next()
+    return Life(input).next()
 
 
-class GameOfLife():
-    def __init__(self, input):
-        self.grid = input
-        self.min_x = 0
+class Life():
+    def __init__(self, start_state):
+        self.grid = []
         self.max_x = 0
-        if len(self.grid) > 0:
-            self.max_x = len(self.grid[0]) - 1
-        self.min_y = 0
+        for y, row in enumerate(start_state):
+            line = []
+            for x, cell in enumerate(row):
+                if cell == '*':
+                    state = State.LIVE
+                else:
+                    state = State.DEAD
+                line.append(Cell(x, y, state))
+            self.grid.append(line)
+            self.max_x = len(line) - 1
         self.max_y = len(self.grid) - 1
+        self._link_cells()
+
+    def _link_cells(self):
+        candidates = [
+            (-1, -1), (0, -1), (1, -1),
+            (-1, 0), (1, 0),
+            (-1, 1), (0, 1), (1, 1)
+        ]
+        for y, row in enumerate(self.grid):
+            for x, cell in enumerate(row):
+                neighbours = []
+                for i, j in candidates:
+                    if self._is_valid(x + i, y + j):
+                        neighbours.append(self.grid[y + j][x + i])
+                cell.link(neighbours)
+
+    def _is_valid(self, x, y):
+        return 0 <= x <= self.max_x and 0 <= y <= self.max_y
 
     def next(self):
-        self.grid = [
-            ''.join([self._cell_status(x, y) for x in range(len(row))])
-            for y, row in enumerate(self.grid)
-        ]
+        for row in self.grid:
+            for cell in row:
+                cell.prepare()
 
-        return self.grid
+        for row in self.grid:
+            for cell in row:
+                cell.next()
 
-    def _cell_status(self, x, y):
-        if self._is_live(x, y):
-            if self._cell_survives(x, y):
-                return LIVE
-            else:
-                return DEAD
+        return self._present()
+
+    def _present(self):
+        grid_str = []
+        for row in self.grid:
+            grid_str.append(
+                ''.join([self._state_str(cell.state) for cell in row]))
+
+        return grid_str
+
+    def _state_str(self, state):
+        if state == State.DEAD:
+            return '.'
         else:
-            if self._is_ripe_for_life(x, y):
-                return LIVE
-            else:
-                return DEAD
+            return '*'
 
-    def _cell_survives(self, x, y):
-        return 2 <= self._neighbours(x, y) <= 3
 
-    def _is_ripe_for_life(self, x, y):
-        return self._neighbours(x, y) == 3
+class Cell():
+    def __init__(self, x, y, state):
+        self.x = x
+        self.y = y
+        self.state = state
+        self.outlook = None
+        self.neighbours = []
 
-    def _neighbours(self, x, y):
-        candidates = [
-            (-1, -1), (-1, 0), (-1, 1),
-            (0, -1), (0, 1),
-            (1, -1), (1, 0), (1, 1),
-        ]
-        count = 0
-        for i, j in candidates:
-            if self._is_valid_cell(x + i, y + j) and \
-                    self._is_live(x + i, y + j):
-                count += 1
+    def link(self, neighbours):
+        self.neighbours = neighbours
 
-        return count
+    def prepare(self):
+        live_neighbours = 0
+        for cell in self.neighbours:
+            if cell.state == State.LIVE:
+                live_neighbours += 1
 
-    def _is_valid_cell(self, x, y):
-        return self.min_x <= x <= self.max_x and \
-            self.min_y <= y <= self.max_y
+        if live_neighbours < 2:
+            self.outlook = Outlook.LONELY
+        elif live_neighbours == 2:
+            self.outlook = Outlook.STABLE
+        elif live_neighbours == 3:
+            self.outlook = Outlook.FERTILE
+        else:
+            self.outlook = Outlook.CROWDED
 
-    def _is_live(self, x, y):
-        return self.grid[y][x] == LIVE
+    def next(self):
+        if self.outlook in [Outlook.LONELY, Outlook.CROWDED]:
+            self.state = State.DEAD
+        elif self.outlook == Outlook.FERTILE:
+            self.state = State.LIVE
+        self.outlook = None
 
-# Second pass. I think I went a little crazy with list comprehensions here;
-# the result being maybe less readable than my first pass.
-# I should have probably added another class for cells: so many of the
-# methods above are for a single cell and it may make sense to
-# isolate that next time.
+
+# Third pass. What a lesson! This code is horrible.
+# The abstractions are wrong and dirty, the workflow is clunky.
+# Not pleasant to read.
+# The reason is that I strayed from TDD, I think, as I wanted to try
+# a different, Cell-based implementation.
+# That was a disaster. I got lost in the weeds for a while, and ended
+# up coding the whole solution mostly in the dark.
+# Good learning on early/wrong abstractions.
