2d7882d61b4e884f04bb057f62dbcf952d1767b0
Jorge Aranda <jorge.aranda@cuevano.ca>
2021-05-20 10:13:07 -0700

    Add another pass of Roman Kata

--- roman/roman.py
+++ roman/roman.py
@@ -2,40 +2,40 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Numeral:
+class RomanNumeral:
     symbol: str
     value: int
 
 
-numerals = [
-    Numeral("M", 1000),
-    Numeral("CM", 900),
-    Numeral("D", 500),
-    Numeral("CD", 400),
-    Numeral("C", 100),
-    Numeral("XC", 90),
-    Numeral("L", 50),
-    Numeral("XL", 40),
-    Numeral("X", 10),
-    Numeral("IX", 9),
-    Numeral("V", 5),
-    Numeral("IV", 4),
-    Numeral("I", 1),
+roman_numerals = [
+    RomanNumeral("M", 1000),
+    RomanNumeral("CM", 900),
+    RomanNumeral("D", 500),
+    RomanNumeral("CD", 400),
+    RomanNumeral("C", 100),
+    RomanNumeral("XC", 90),
+    RomanNumeral("L", 50),
+    RomanNumeral("XL", 40),
+    RomanNumeral("X", 10),
+    RomanNumeral("IX", 9),
+    RomanNumeral("V", 5),
+    RomanNumeral("IV", 4),
+    RomanNumeral("I", 1),
 ]
 
 
 def to_roman(arabic: int) -> str:
-    for numeral in numerals:
+    for numeral in roman_numerals:
         if numeral.value <= arabic:
             return numeral.symbol + to_roman(arabic - numeral.value)
     return ""
 
 
 def to_arabic(roman: str) -> int:
-    for numeral in numerals:
+    for numeral in roman_numerals:
         if roman.startswith(numeral.symbol):
             return numeral.value + to_arabic(roman[len(numeral.symbol) :])
     return 0
 
 
-# Sixth pass. Minuscule change this time.
+# Seventh pass. Minimal naming changes only; clean progression.
